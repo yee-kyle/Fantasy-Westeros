5b9244b0ff3615a6775f7988f7d9c90fb7fb6ce8
Yee, Kyle <kyle.yee@accenture.com>
2019-07-12 13:18:42 -0400

    feature(register): add register page functionality and remove styles.css from each folder

--- src/components/register-page/index.jsx
+++ src/components/register-page/index.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import styles from './styles.css';
-
-export const text = {
-  headline: 'Fantasy Quidditch',
-  greeting: 'Hogwart Quidditch Roster Manager',
-};
 
 const RegisterPage = () => (
-  <main className={styles.root}>
-    <h1 className={styles.headline}>{text.headline}</h1>
-    <section>
-      <span className={styles.greeting}>{text.greeting}</span>
-    </section>
+  <main className="registerPage">
+    <form action="https://players-api.developer.alchemy.codes/api/user" method="post">
+      <fieldset className="inputFields">
+        <div className="registerField">
+          <label className="registerLabel">First Name:</label>
+          <input id="firstName" className="registerInput" alt="FirstName" name="firstName" type="text" />
+        </div>
+        <div className="registerField">
+          <label className="registerLabel">Last Name:</label>
+          <input id="lastName" className="registerInput" alt="LastName" name="lastName" type="text"/>
+        </div>
+        <div className="registerField">
+          <label className="registerLabel">Email Address:</label>
+          <input id="email" className="registerInput" alt="Email" name="email" type="email"/>
+        </div>
+        <div className="registerField">
+          <label className="registerLabel">Password:</label>
+          <input id="password" className="registerInput" alt="Password" name="password" type="password"/>
+        </div>
+        <div className="registerField">
+          <label className="registerLabel">Confirm Password:</label>
+          <input id="confirmPassword" className="registerInput" alt="passwordConfirm" name="confirmPassword" type="password"/>
+        </div>
+        <div className="registerField">
+          <input id="register" className="registerButton" type="submit" value="Register"/>
+        </div>
+      </fieldset>
+    </form>
   </main>
 );
 
